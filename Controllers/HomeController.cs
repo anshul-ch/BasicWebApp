425c1da98a5f4e4ad323f015a1bf3b8ad6645a0c
Anshul <Anshul@DESKTOP-A2619BA>
2026-02-25 12:40:18 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Student> StudentList = new List<Student>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                StudentList.Add(student);
+                ViewBag.Message = "Form submitted successfully!";
+                return View("Index");
+            }
+
+            return View("Index", student);
+        }
+
+        public IActionResult ViewStudents()
+        {
+            return View(StudentList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        //[Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        //[Required(ErrorMessage = "Email is mandatory")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        //[Required(ErrorMessage = "Message is required")]
+        [StringLength(200)]
+        public string Message { get; set; }
+    }
+}
